45a3314d4a1036315adacc4c07b2134341fd06d2
Aaron Rudzki <aaronrudzki@quickenloans.com>
2020-07-06 16:57:22 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Lab_2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Enter the length of the room"); // user inputs length data
+            string entry = Console.ReadLine();
+            double length = double.Parse(entry);
+            Console.WriteLine($"Enter the width of the room"); // user inputs width data
+            string width_entry = Console.ReadLine();
+            double width = double.Parse(width_entry);
+            double area = length * width;
+            Console.WriteLine($"Area = {area}");
+            double perimeter = (length + width) * 2;
+            Console.WriteLine($"Perimeter = {perimeter}");
+            if (area < 250)
+            {
+                Console.WriteLine($"This is a small-sized room");
+            }
+            else if (area > 250 && area < 650)
+            {
+                Console.WriteLine($"This is a medium-sized room");
+            }
+            else if (area > 650)
+            {
+                Console.WriteLine($"This is a large-sized room");
+            }
+        }
+    }
+}
